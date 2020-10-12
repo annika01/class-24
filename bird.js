8896b5a5b0837240476f515ed67b335fa837fc41
annika01 <71920315+annika01@users.noreply.github.com>
2020-10-12 17:48:06 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,24 @@
+class Bird{
+    constructor(x,y){
+        var options= {
+            density: 1.5,
+            friction: 1,
+            restitution: 0.5 
+        }
+        this.body= Bodies.rectangle(x,y,50,50,options)
+World.add(world, this.body)
+    }
+    display(){
+        var pos= this.body.position
+pos.x= mouseX
+pos.y= mouseY
+var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("red");
+    rect(0, 0, 50, 50);
+    pop();
+    }
+}
